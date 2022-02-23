7d1de4ea2b8f620f8ce959a76b03d67e3ad03628
Mark Fleschler <mark.f.accts@gmail.com>
2022-02-23 00:25:20 -0800

    Added sketch 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,9 @@ import java.awt.Color
 
 //fun main() = PApplet.main(Hexbounce1::class.java.name)
 //fun main() = PApplet.main(Hexbounce2::class.java.name)
+fun main() = PApplet.main(Hexbounce3::class.java.name)
 //fun main() = PApplet.main(Hexbounce4::class.java.name)
-fun main() = PApplet.main(Hexbounce5::class.java.name)
+//fun main() = PApplet.main(Hexbounce5::class.java.name)
 
 fun Int.rgb() : Int {
     return Color(this).rgb
